2cf5ff3d14f00b5dc096b219b05034b1467b8824
EC2 Default User <ec2-user@ip-172-31-89-58.ec2.internal>
2022-01-08 05:19:57 +0000

    [Finish]コメント機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_07_231514) do
+ActiveRecord::Schema.define(version: 2022_01_08_022549) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
