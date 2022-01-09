a9a23577cddba1c3d3b047b014e2b4e1c816ece5
EC2 Default User <ec2-user@ip-172-31-89-58.ec2.internal>
2022-01-09 14:22:16 +0000

    [Update]細かい修正とviewへの実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,12 +1,12 @@
 class RelationshipsController < ApplicationController
 
   def create
-    corrent_user.follow(params[:user_id])
+    current_user.follow(params[:user_id])
     redirect_to request.referer
   end
 
   def destroy
-    corrent_user.unfollow(params[:user_id])
+    current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
 
